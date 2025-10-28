d027da18b4fc76a63996c110499328c164437a38
SUP3RMASS1VE <Martincull@live.com>
2025-10-28 20:06:02 +0000

    Update install.js to include additional shell commands

--- install.js
+++ install.js
@@ -5,11 +5,23 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/ace-step/ACE-Step app",
+          "git clone https://github.com/ace-step/ACE-Step app"
         ]
       }
     },
     // Delete this step if your project does not use torch
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install gradio torch",
+          "uv pip install -e ."
+        ]
+      }
+    },
+    // Edit this step with your custom install commands
     {
       method: "script.start",
       params: {
@@ -18,22 +30,10 @@ module.exports = {
           venv: "env",                // Edit this to customize the venv folder path
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
+          //  triton: true   // uncomment this line if your project requires triton
           // sageattention: true   // uncomment this line if your project requires sageattention
         }
       }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "uv pip install gradio devicetorch",
-          "uv pip install -e ."
-        ]
-      }
-    },
+    }
   ]
 }
